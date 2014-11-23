180b0e79cf8e57b71c1bdc2a503c529925c88798
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-11-23 14:05:03 -0600

    Se eliminaron las tablas sin usar

--- recursos/BD.sql
+++ recursos/BD.sql
@@ -30,38 +30,3 @@ CREATE TABLE Actividades(
 	Competencias	TEXT(50)	NOT NULL,
 	PRIMARY KEY (Id)
 );
-
-DROP TABLE IF EXISTS Tipos;
-CREATE TABLE Tipos(
-	Id			INT			NOT NULL	AUTO_INCREMENT,
-	Categoria	INT			NOT NULL,
-	Nombre		TEXT(50)	NOT NULL,
-	PRIMARY KEY (Id)
-);
-
-DROP TABLE IF EXISTS Roles;
-CREATE TABLE Roles(
-	Id	INT	NOT NULL AUTO_INCREMENT,
-	Rol	INT	NOT NULL,
-	PRIMARY KEY (Id)
-);
-
-DROP TABLE IF EXISTS Periodos;
-CREATE TABLE Periodos(
-	Id		INT	NOT NULL AUTO_INCREMENT,
-	Periodo	INT	NOT NULL,
-	PRIMARY KEY (Id)
-);
-
-DROP TABLE IF EXISTS AreasImpacto;
-CREATE TABLE AreasImpacto(
-	Id			INT			NOT NULL AUTO_INCREMENT,
-	AreaImpacto	TEXT(50)	NOT NULL,
-	PRIMARY KEY (Id)
-);
-
-INSERT INTO Roles(Rol) VALUES('Organizador'), ('Colaborador'), ('Participante');
-
-INSERT INTO Periodos(Periodo) VALUES('Bimestral Sep - Nov de 2014'), ('Marzo - Noviembre de 2014'), ('SemestralAgo - Dic de 2014'), ('Semestral Ene - May de 2015'), ('Septiembre 2014 - Marzo 2015'), ('Tetramestral May - Ago 2014'), ('Tetramestral Sep - Dic 2014'), ('Tetramestre Sep-Dic 2014'), ('Trimestral Ene - Abr de 2015'), ('Trimestral Sep - Dic de 2014');
-
-INSERT INTO AreasImpacto(AreaImpacto) VALUES('Social'), ('Ambiental'), ('Económico');
