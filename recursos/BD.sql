b87c6342578eaaad58228716da49e42435530222
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-11-23 00:49:42 -0600

    Errores en la base de datos
    Se corrigieron los archivos en recursos/BD.sql

--- recursos/BD.sql
+++ recursos/BD.sql
@@ -1,19 +1,19 @@
+DROP DATABASE IF EXISTS DAEITESMCCV;
 CREATE DATABASE DAEITESMCCV;
 USE DAEITESMCCV;
 
 DROP TABLE IF EXISTS Alumnos;
 CREATE TABLE Alumnos(
-	Matricula   	TEXT(9)	NOT NULL,
-	Nombre      	TEXT   	NOT NULL,
-	Apellido    	TEXT   	NOT NULL,
-	PRIMARY KEY (Matricula)
+	Matricula   	TEXT(9)  	NOT NULL,
+	Nombre      	TEXT(50)   	NOT NULL,
+	Apellido    	TEXT(50)   	NOT NULL,
+	Password		TEXT(50)	NOT NULL
 );
 
 DROP TABLE IF EXISTS Administradores;
 CREATE TABLE Administradores(
 	Nomina      	TEXT(9)	NOT NULL,
-	Password    	TEXT   	NOT NULL,
-	PRIMARY KEY (Nomina)
+	Password    	TEXT(50)   	NOT NULL
 );
 
 DROP TABLE IF EXISTS Actividades;
@@ -21,13 +21,13 @@ CREATE TABLE Actividades(
 	Id          	INT    	NOT NULL,
 	Alumno      	INT    	NOT NULL,
 	Tipo        	INT    	NOT NULL,
-	Nombre      	TEXT   	NOT NULL,
-	Descripcion 	TEXT   	NOT NULL,
+	Nombre      	TEXT(50)   	NOT NULL,
+	Descripcion 	TEXT(50)   	NOT NULL,
 	Rol         	INT    	NOT NULL,
 	Periodo     	INT    	NOT NULL,
 	AreaImpacto 	INT    	NOT NULL,
-	Aprendizajes	TEXT   	NOT NULL,
-	Competencias	TEXT   	NOT NULL,
+	Aprendizajes	TEXT(50)   	NOT NULL,
+	Competencias	TEXT(50)   	NOT NULL,
 	PRIMARY KEY (Id)
 );
 
@@ -35,7 +35,7 @@ DROP TABLE IF EXISTS Tipos;
 CREATE TABLE Tipos(
 	Id          	INT    	NOT NULL AUTO_INCREMENT,
 	Categoria   	INT    	NOT NULL,
-	Nombre      	TEXT   	NOT NULL,
+	Nombre      	TEXT(50)   	NOT NULL,
 	PRIMARY KEY (Id)
 );
 
@@ -56,12 +56,12 @@ CREATE TABLE Periodos(
 DROP TABLE IF EXISTS AreasImpacto;
 CREATE TABLE AreasImpacto(
 	Id          	INT    	NOT NULL AUTO_INCREMENT,
-	AreaImpacto 	TEXT   	NOT NULL,
+	AreaImpacto 	TEXT(50)   	NOT NULL,
 	PRIMARY KEY (Id)
 );
 
-INSERT INTO Roles VALUES('Organizador'), ('Colaborador'), ('Participante');
+INSERT INTO Roles(Rol) VALUES('Organizador'), ('Colaborador'), ('Participante');
 
-INSERT INTO Periodos VALUES('Bimestral Sep - Nov de 2014'), ('Marzo - Noviembre de 2014'), ('Semestral  Ago - Dic de 2014'), ('Semestral Ene - May de 2015'), ('Septiembre 2014 - Marzo 2015'), ('Tetramestral May - Ago 2014'), ('Tetramestral Sep - Dic 2014'), ('Tetramestre Sep-Dic 2014'), ('Trimestral Ene - Abr de 2015'), ('Trimestral Sep - Dic de 2014');
+INSERT INTO Periodos(Periodo) VALUES('Bimestral Sep - Nov de 2014'), ('Marzo - Noviembre de 2014'), ('Semestral  Ago - Dic de 2014'), ('Semestral Ene - May de 2015'), ('Septiembre 2014 - Marzo 2015'), ('Tetramestral May - Ago 2014'), ('Tetramestral Sep - Dic 2014'), ('Tetramestre Sep-Dic 2014'), ('Trimestral Ene - Abr de 2015'), ('Trimestral Sep - Dic de 2014');
 
-INSERT INTO AreasImpacto VALUES('Social'), ('Ambiental'), ('Económico');
+INSERT INTO AreasImpacto(AreaImpacto) VALUES('Social'), ('Ambiental'), ('Económico');
