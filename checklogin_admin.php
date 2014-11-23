2a2890228b632e1e1520b6014c278caf694d590c
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-11-22 20:51:26 -0600

    Commit inicial
    Se tienen los cambios hechos por MacMau y Aurioles

---
+++ checklogin_admin.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	if ($username == "A00123456" && $password == "A00123456"){
+		$_SESSION["username"] = $username;
+		header("location:homeadmin.html");
+
+	}
+	else{
+		header("location:indexadmin.html?error=true");
+	}
+?>
