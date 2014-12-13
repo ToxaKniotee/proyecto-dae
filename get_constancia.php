de3ea75151ca590bf0e46bb12780e5a11c284276
Toxa Kniotee <toxa_kniotee@outlook.com>
2014-12-13 16:36:56 -0600

    Primera version de constancias sin LaTeX
    Falta completar el espaciado correcto y la incursión de imágenes

--- get_constancia.php
+++ get_constancia.php
@@ -16,91 +16,79 @@ if ($result->num_rows > 0) {
     $apellido = $row[1];
 }
 
-/* Creamos un archivo tex */
-$tex_file = fopen('/toxa/constancia.tex', 'w');
-fwrite(
-    $tex_file,
-    "\\documentclass{letter}
+/* Incluimos la librería para generar PDFs desde php */
+require_once('assets/tcpdf/tcpdf.php');
 
-\\textheight    = 20cm
-\\textwidth     = 18cm
-\\topmargin     = -2cm
-\\oddsidemargin = -1cm
-\\parindent     =  0mm
+/* Configuración básica para crear un archivo pdf sin header y footer */
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-\\usepackage{amsmath, amssymb, amsfonts, latexsym}
-\\usepackage[T1]{fontenc}
-\\usepackage[utf8]{inputenc}
-\\usepackage[spanish]{babel}
-\\usepackage{graphicx}
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('ITESM CCV');
+$pdf->SetTitle('Constancia');
+$pdf->SetSubject('Constancia de actividades DAE');
+$pdf->SetKeywords('Constancia, dae, itesm, ccv');
 
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
 
-\\begin{document}
-\\pagestyle{empty}
-\\begin{center}
-\\hspace{-14.5cm}\includegraphics[width=2cm]{images/dae.jpg}
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
-\\hspace{10.5cm}H. Córdoba, Ver. a 23 de septiembre de 2014
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 
+// set auto page breaks
+$pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
 
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
-
-\\end{center}
-
-\\vspace{1.2cm}A quien corresponda
-
-\\vspace{1.5cm}El motivo de la presente, y para los fines del interesado, es hacer constar que el alumno {\\bf $nombre $apellido}, matrícula {\\bf $matricula} -quien se encuentra cursando el quinto semestre de sus estudios profesionales en el Tecnológico de Monterrey Campus Central de Veracruz- destacándose como una persona responsable y capaz, no solo en el desempeño de sus actividades académicas sino también a través de la participación en diferentes actividades extraacadémicas, participando activamente en Difusión Cultural, Deportes y Grupos Estudiantiles durante toda su estancia. 
-
-Por este motivo y por la determinación que $nombre $apellido siempre ha mostrado, me permito recomendarlo - como Director de Asuntos Estudiantiles – como un alumno que participa activamente en las actividades extracurriculares, quedando a sus órdenes para corroborar la presente información. 
-
-Agradezco de antemano la atención que sirvan prestar a la presente. 
-
-\\begin{center}
-
-\\vspace{1.2cm}Atentamente.
-
-Lic. Juan F. Franco Zanatta
-
-\\includegraphics[width=2cm]{images/firma.jpg}
-
-Director de Asuntos Estudiantiles
-
-Dirección General
-
-Campus Central de Veracruz
-
-TECNOLÓGICO DE MONTERREY
-
-juan.franco@itesm.mx
-
-http://www.itesm.mx
-
-Tel. +52 (271) 717 0514, ext. 6620
-\\end{center}
-
-
-\\end{document}"
-);
-fclose($tex_file);
-
-/* Compilamos el latex */
-system('cd /toxa; pdflatex constancia.tex', $var);
-
-/* Obtenemos el archivo generado */
-$file = fopen('/toxa/constancia.pdf', 'r');
-
-/* Regresamos el archivo */
-$file = '/toxa/constancia.pdf';
-if (file_exists($file)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    ob_clean();
-    flush();
-    readfile($file);
-    exit;
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+    require_once(dirname(__FILE__).'/lang/eng.php');
+    $pdf->setLanguageArray($l);
 }
+
+// set font
+$pdf->SetFont('times', '', 12);
+
+// add a page
+$pdf->AddPage();
+
+$html = '
+<p align="right">H. Córdoba, Ver. a GET FECHA</p>
+
+<p>A quien corresponda:</p>
+
+<p align="justify">El motivo de la presente, y para los fines del interesado, es hacer constar que el alumno
+<strong>'.$nombre. ' '.$apellido.' </strong>, matrícula <strong>'.$matricula.'</strong> -quien se encuentra cursando el
+quinto semestre de sus estudios profesionales en el Tecnológico de Monterrey Campus Central de Veracruz- destacándose
+como una persona responsable y capaz, no solo en el desempeño de sus actividades académicas sino también a través de
+la participación en diferentes actividades extraacadémicas, participando activamente en Difusión Cultural, Deportes y
+Grupos Estudiantiles durante toda su estancia.</p>
+
+<p align="justify">Por este motivo y por la determinación que '.$nombre.' '.$apellido.' siempre ha mostrado, me permito
+recomendarlo - como Director de Asuntos Estudiantiles – como un alumno que participa activamente en las actividades
+extracurriculares, quedando a sus órdenes para corroborar la presente información.</p>
+
+<p>Agradezco de antemano la atención que sirvan prestar a la presente.</p>
+
+<p align="center">Atentamente
+<br />Lic. Juan F. Franco Zanatta
+<br />Director de Asuntos Estudiantiles
+<br />Dirección General
+<br />Campus Central de Veracruz
+<br />TECNOLÓGICO DE MONTERREY</p>
+
+<p align="center">juan.franco@itesm.mx
+<br />http://www.itesm.mx</p>
+
+<p align="center">Tel. +52 (271) 717 0514, ext. 6620</p>
+';
+
+$pdf->writeHTML($html, true, false, true, false, '');
+
+$pdf->Output('Constancia', 'I');
